6f189501260eac43ed9cf398c42f1048eb512e18
Khuram <knshaikh@gmail.com>
2017-06-19 12:26:55 -0600

    export statements

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyDa6IX9K5lsxoI2JcRvK5_VxWB6FdvPMec';
 
 //Step 1: Make the component
 const App = () => {
-    return <div>Hi!</div>;
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
 }
 //Step 2: Show it on the page(in DOM)
 
